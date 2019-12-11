624f1ba8d5c016f5654a0a3f59e77a760794b426
rizalin <55905761+rizalin@users.noreply.github.com>
2019-12-11 21:33:18 +0700

    Create login.js

---
+++ src/login.js
@@ -0,0 +1,21 @@
+import React , { Component } from 'react';
+
+
+import './App.css';
+
+class Login extends Component {
+constructor(props){
+    super(props);
+}
+
+render() {
+ return (
+ <div>
+ 
+ <h1>Ini Login</h1>
+ </div>
+ );
+ }
+}
+
+export default Login;
