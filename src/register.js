456a66313faa60048da122d627a966b6a87160fc
rizalin <55905761+rizalin@users.noreply.github.com>
2019-12-11 21:51:46 +0700

    Update register.js

---
+++ src/register.js
@@ -0,0 +1,21 @@
+import React , { Component } from 'react';
+
+
+import './App.css';
+
+class Register extends Component {
+constructor(props){
+    super(props);
+}
+
+render() {
+ return (
+ <div>
+ 
+ <h1>Ini Register</h1>
+ </div>
+ );
+ }
+}
+
+export default Register;
