c756d2bf61579cee2e50e41a89ac1dce8a85e622
rizalin <55905761+rizalin@users.noreply.github.com>
2019-12-12 15:26:55 +0700

    Add files via upload

--- src/App.js
+++ src/App.js
@@ -6,18 +6,32 @@ import './App.css';
 
 function Follow() {
   return (
-    <Media className='profileBr'>
-      <Avatar style={{ height: '75px', width: '75px', marginRight: '25px' }}>A</Avatar>
-      <Media.Body style={{ paddingRight: '100px' }}>
-        <h3>WRITTEN BY</h3>
-        <h2>Ryan Holiday</h2>
-        <p>
-          Bestselling author of 'Conspiracy', 'Ego is the enemy' & 'The obstacle Is The Way'
-          http://amzn.to/24qKRWR
+    <div>
+      <Media className='profileTop'>
+        <Avatar style={{ height: '75px', width: '75px', marginRight: '25px' }}>A</Avatar>
+        <Media.Body style={{ paddingRight: '100px' }}>
+          <h3>WRITTEN BY</h3>
+          <h2>Ryan Holiday</h2>
+          <p>
+            Bestselling author of 'Conspiracy', 'Ego is the enemy' & 'The obstacle Is The Way'
+            http://amzn.to/24qKRWR
             </p>
-      </Media.Body>
-      <Button variant="outline-success" size="sm" style={{ marginTop: 'auto', marginBottom: 'auto' }}>Follow</Button>
-    </Media>
+        </Media.Body>
+        <Button variant="outline-success" size="sm" style={{ marginTop: 'auto', marginBottom: 'auto' }}>Follow</Button>
+      </Media>
+      <Media className='profileBottom'>
+        <Avatar style={{ height: '75px', width: '75px', marginRight: '25px' }}>A</Avatar>
+        <Media.Body style={{ paddingRight: '100px' }}>
+          <h3>WRITTEN BY</h3>
+          <h2>Ryan Holiday</h2>
+          <p>
+            Bestselling author of 'Conspiracy', 'Ego is the enemy' & 'The obstacle Is The Way'
+            http://amzn.to/24qKRWR
+          </p>
+        </Media.Body>
+        <Button variant="outline-success" size="sm" style={{ marginTop: 'auto', marginBottom: 'auto' }}>Follow</Button>
+      </Media>
+    </div>
   );
 }
 
